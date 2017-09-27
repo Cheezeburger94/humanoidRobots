384087294cda299052f1ff37caaceb1b7ced01bc
Cheezeburger94 <andi199424@hotmail.com>
2017-09-27 17:51:30 +0200

    Updated motor hsc

--- main.m
+++ main.m
@@ -5,9 +5,9 @@ clear all;
 hsc_motor = MotorHSC()
 
 %enableTorque(hsc_motor,1)
-%hsc_motor.moveToGoalPosition(hsc_motor,1,500)
+hsc_motor.moveToPosition(hsc_motor,7,0.6)
+hsc_motor.moveToPosition(hsc_motor,8,0.6)
 %
-%hsc_motor.readPresentPosition(hsc_motor,1)
-hsc_motor.enableTorque(hsc_motor,2)
+hsc_motor.readPosition(hsc_motor,8)
 
 hsc_motor.closeConnection(hsc_motor);
